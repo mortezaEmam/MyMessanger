d2fcf86f0afa18eab36d43e9d15cb5cbd9f199a1
morteza emamverdi <morteza.emamverdi1372@gmail.com>
2023-03-26 21:28:54 +0430

    create message and conversation model and tables

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/',function (){
     return view('welcome');
-});
+})->middleware('auth:web');
