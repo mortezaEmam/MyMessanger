c3cc0db20cdedba5c57dffb5fc3c2f7d35958c0e
morteza emamverdi <morteza.emamverdi1372@gmail.com>
2023-03-27 06:59:43 +0430

    insert css and html chatlist

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,4 +1,40 @@
 <div class="test">
-   <div class="chatlist_header"></div>
-    <div class="chatlist_body"></div>
+    <div class="chatlist_header">
+        <div class="title">
+            chat
+        </div>
+        <div class="img_container">
+            <img src="{{asset('img/index_top_1.png')}}" alt="">
+        </div>
+    </div>
+    <div class="chatlist_body">
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
+                <img src="{{asset('img/slider_1.jpg')}}" alt="">
+            </div>
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">morteza</div>
+                    <span class="date">2d</span>
+                </div>
+                <div class="bottom_row">
+                    <div class="message_body text-truncate">
+                        ipsom gdfhddhdhdhdhdhd ghhhhhhhhhhhhhhhhhh dfffffffffffffffffffff
+                        ipsomipsom ipsom
+                        ipsomddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd ipsom
+                        ipsomdddddddddddddddd ipsomdddddddddddddddddddddddddddddddddddddd
+                        ipsomdddddddddddipsom gdfhddhdhdhdhdhd ghhhhhhhhhhhhhhhhhh dfffffffffffffffffffff ipsom
+                        ipsomddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+                        ipsom ipsomdddddddddddddddd ipsomdddddddddddddddddddddddddddddddddddddd
+                        ipsomddddddddddd ipsomdddddddddddddddd
+                        ipsomdddddddddddddddddddddddddddddddddddddd
+                        ipsomddddddddddd ipsom gdfhddhdhdhdhdhd ghhhhhhhhhhhhhhhhhh dfffffffffffffffffffff
+                        ipsomddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+
+                    </div>
+                    <div class="unread_count">56</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
